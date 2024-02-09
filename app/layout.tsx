73f48339878fa3d0a8bffa25ca8b1dac5ba681be
Cyrielle Thomas <cyriellethomas@Cyrielle.local>
2024-02-09 11:30:43 +0100

    Ajout image metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,44 @@
-
-import './globals.css'
-import type { Metadata } from 'next'
-import { Arapey } from 'next/font/google'
-import localFont from 'next/font/local'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Arapey } from "next/font/google";
+import localFont from "next/font/local";
 
 const beautiful = localFont({
-  src: '../fonts/beautiful.woff2',
-  display: 'swap',
-  variable:'--font-beautiful'
-})
+  src: "../fonts/beautiful.woff2",
+  display: "swap",
+  variable: "--font-beautiful",
+});
 
 const arapey = Arapey({
-  subsets :['latin'],
+  subsets: ["latin"],
   // style :['normal'],
-  variable :'--font-arapey',
-  weight : ['400'],
-})
+  variable: "--font-arapey",
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: {
-    default : 'Cyrielle Thomas - Développeur web freelance ',
-   template : '%s | Cyrielle Thomas - Développeur web freelance'}
-  ,
-  description: 'Création de site web sur-mesure',
-}
-
-
+    default: "Cyrielle Thomas - Développeuse web freelance ",
+    template: "%s | Cyrielle Thomas - Développeur web freelance",
+  },
+  description: "Création de site web sur-mesure",
+  openGraph: {
+    images: "https://ibb.co/PDw9xyC",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className ={` ${beautiful.variable} ${arapey.className} bg-background text-xl`}>{children}</body>
+      <body
+        className={` ${beautiful.variable} ${arapey.className} bg-background text-xl`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
